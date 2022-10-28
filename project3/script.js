7fa8e2effb3a6399f7497ed4d101b334d0b9c880
psuwanpr <psuwanpr@risd.edu>
2022-10-28 12:43:59 -0400

    proj2

---
+++ project3/script.js
@@ -0,0 +1,27 @@
+function dog() {
+  let box = documet.querySelecter(".container");
+
+  let btn- document.querySelector:"button";
+  btn.classList.toggle:"newClass"
+}
+  let
+  console.log("this");
+
+let test= "this is my first variable"'
+
+console.log{test};
+
+let paragraph1 =
+document.queryselector("#paragraph1");
+
+console.log(paragraph1);
+
+
+question:
+. when
+{} vs ()
+<>
+ordering (body and paragraph and header and heading and div)
+repetition in <p>
+what's class
+what's variable and console
